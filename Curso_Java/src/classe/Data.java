b55a21943d4a76d29c7cbf7f53fbd39ba5ca0cef
Geovane Maia <dartanha272@gmail.com>
2020-07-15 10:28:27 -0300

    Usando o This e This()

--- Curso_Java/src/classe/Data.java
+++ Curso_Java/src/classe/Data.java
@@ -2,8 +2,30 @@ package classe;
 
 public class Data {
 
-	public Data(int i, int j, int k) {
-		
+	int dia;
+	int mes;
+	int ano;
+
+	Data() {
+		//dia = 1;
+		//mes = 1;
+		//ano = 1970;
+		this(1,1,1970);
+
+	}
+
+	Data(int dia,int mes,int ano){
+		this.dia = dia;
+		this.mes = mes;
+		this.ano = ano;
+	}
+
+	String obterDataFormatada() {
+		return String.format("%d/%d/%d",dia,mes,ano);
+	}
+
+	void imprimirDataFormatada(){
+		System.out.println(obterDataFormatada());
 	}
 
 }
