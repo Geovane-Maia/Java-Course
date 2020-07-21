b419a73e196f2315c8ad92249a2fec5baab05274
Geovane Maia <dartanha272@gmail.com>
2020-07-21 08:48:13 -0300

    Exercicio(Comidas)

---
+++ Curso_Java/src/desafio/Pessoa.java
@@ -0,0 +1,24 @@
+package desafio;
+
+public class Pessoa {
+	
+	String nome;
+	double peso;
+	
+	Pessoa(String nome,double peso){
+		
+		this.nome = nome;
+		this.peso = peso;
+	}
+	
+	void Comer(Comida comida) {
+		if(comida != null) {
+			this.peso += comida.peso;
+		}
+		
+	}
+	String apresentar() {
+		return "Olá eu me chamo "+ nome +" e peso "+ peso +"Kg.";
+	}
+
+}
