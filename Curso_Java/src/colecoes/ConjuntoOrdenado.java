1c2a2a4190e42ce95277cac979f53122e13593d4
Geovane Maia <dartanha272@gmail.com>
2020-07-28 08:33:55 -0300

    Aprendendo HashSet e Set

---
+++ Curso_Java/src/colecoes/ConjuntoOrdenado.java
@@ -0,0 +1,19 @@
+package colecoes;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class ConjuntoOrdenado {
+
+	public static void main(String[] args) {
+		
+		Set<String> lista = new HashSet<String>();
+		
+		lista.add("Geovane");
+		System.out.println(lista);
+		
+		
+
+	}
+
+}
