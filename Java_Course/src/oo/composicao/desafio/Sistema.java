f3eb3ed66a7d6d4849d1108d3a9b8fa0acd5a1ca
Geovane Maia <dartanha272@gmail.com>
2020-08-13 21:13:25 -0300

    Falta resolver um problema na conta !

--- Java_Course/src/oo/composicao/desafio/Sistema.java
+++ Java_Course/src/oo/composicao/desafio/Sistema.java
@@ -4,7 +4,21 @@ public class Sistema {
 
 	public static void main(String[] args) {
 		
-		System.out.println("");
+		
+		
+		Compra compra1 = new Compra();
+		compra1.adcionarItem("Caneta", 1, 100);
+		compra1.adcionarItem(new Produto("Notebook",2000),2);
+		
+		Compra compra2 = new Compra();
+		compra2.adcionarItem("Caderno", 10, 10);
+		compra2.adcionarItem(new Produto("Impressora", 1000),1);
+		
+		Cliente cliente = new Cliente("Geovane Maia");
+		cliente.compras.add(compra1);
+		cliente.compras.add(compra2);
+		
+		System.out.println("O valor total das compras foi R$ " + cliente.obterValorTotal());
 
 	}
 
