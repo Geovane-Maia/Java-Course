d5f5dbda2159c270c4bb09995e3d77cff38f997e
Geovane Maia <dartanha272@gmail.com>
2020-09-22 22:30:03 -0300

    Lambdas Foreach Funcional

--- Java_Course/src/lambdas/Calculo.java
+++ Java_Course/src/lambdas/Calculo.java
@@ -1,10 +1,17 @@
 package lambdas;
 
+@FunctionalInterface
 public interface Calculo {
 	
 	public abstract double executar (double a, double b);
 	
+	default String legal() {
+		return "Legal";
+	}
 	
+	static String muitoLegal() {
+		return "Muito legal ";
+	}
 		
 
 }
