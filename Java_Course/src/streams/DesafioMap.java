661cf0ceda8fec8e1e007c13f1e2c25543313b69
Geovane-Maia <dartanha272@gmail.com>
2020-11-05 21:53:03 -0300

    Usando Streams e Map

---
+++ Java_Course/src/streams/DesafioMap.java
@@ -0,0 +1,22 @@
+package streams;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class DesafioMap {
+
+	public static void main(String[] args) {
+		
+		List<Integer> nums = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9);
+		
+		/*
+		 * 1. Número para String Binária... 6 => "110"
+		 * 2. Inverter a String "110" => "011"
+		 * Converter de volta para inteiro => "011" => 3
+		 * 
+		 * Integer.
+		 */
+
+	}
+
+}
